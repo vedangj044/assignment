fc0cf7c94df3fc8c4fc684ea9fb5f3b91088a588
vedangj044 <vedangj044@gmail.com>
2019-07-20 16:18:34 +0530

    Changes

--- java_assign/a.java
+++ java_assign/a.java
@@ -1,14 +1,20 @@
 class a{
   public static void main(String args[]){
     for(int i=0; i<15; i++){
-      for(int j=0; j<50; j++){
+      for(int j=0; j<40; j++){
         if(j<=12 && i<=9){
-          if(i%2!=0 && j==12){
-            System.out.print("=");
+          if(i%2!=0){
+            if(j==12){
+              System.out.print("=");
+            }
+            else{
+              System.out.print(" * ");
+              j=j+2;
+            }
           }
           else{
-            System.out.print(" *");
-            j++;
+            System.out.print("*  ");
+            j=j+2;
           }
         }
         else{
